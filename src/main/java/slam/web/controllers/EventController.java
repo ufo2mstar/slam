66dbe3daf1a508e154edb20b127d3f54ea66041c
Anthony C <acuevas.15@gmail.com>
2016-05-14 17:48:07 -0400

    adding logging configs. Presently, configs are throwing an error

--- src/main/java/slam/web/controllers/EventController.java
+++ src/main/java/slam/web/controllers/EventController.java
@@ -4,6 +4,8 @@ import java.util.UUID;
 
 import javax.validation.Valid;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,10 +16,14 @@ import slam.model.Event;
 
 @Controller
 public class EventController {
+	 
+	private static final Logger log = LoggerFactory.getLogger(EventController.class);
 
     @RequestMapping(path="event", method=RequestMethod.POST)
     @ResponseBody
     public UUID postEvent(@RequestBody @Valid Event event) {
+    	log.debug("recieved an event: " + event);
+    	
         return UUID.randomUUID();
     }
 
