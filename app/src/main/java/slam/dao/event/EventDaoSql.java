aa350be3579fcda9502371a649e67a90b81377cc
Naren SivaSubramani <ufo2mstar@gmail.com>
2016-05-26 01:23:06 -0400

    QTN: here are some questions and recommendations going fwd

--- app/src/main/java/slam/dao/event/EventDaoSql.java
+++ app/src/main/java/slam/dao/event/EventDaoSql.java
@@ -16,33 +16,49 @@ import slam.model.Event;
 
 @Component
 public class EventDaoSql extends NamedParameterJdbcDaoSupport {
-	
-	private static final String INSERT = "INSERT INTO events (id, correlationId) VALUES (?, ?)";
 
-	private static final String FIND_BY_ID = "SELECT * FROM events where id = :id";
-	
-	@Autowired
-	public void setTemplate(JdbcTemplate jdbcTemplate) {
-		super.setJdbcTemplate(jdbcTemplate);
-	}
-	
-	public void insert(final Event event) {
-		this.getJdbcTemplate().update(INSERT, new PreparedStatementSetter() {
+    private static final String INSERT = "INSERT INTO events (id, correlationId) VALUES (?, ?)";
 
-			@Override
-			public void setValues(PreparedStatement stmt) throws SQLException {
-				stmt.setString(1, event.getId().toString());
-				stmt.setString(2, event.getCorrelationId().toString());
-			}
-			
-		});
-	}
-	
-	public Event find(UUID id) {
-		SqlParameterSource params = new MapSqlParameterSource("id", id.toString());
-		
-		return this.getNamedParameterJdbcTemplate()
-			.queryForObject(FIND_BY_ID, params, new EventMapper());
-	}
-	
+    private static final String FIND_BY_ID = "SELECT * FROM events where id = :id";
+
+    @Autowired
+    public void setTemplate(JdbcTemplate jdbcTemplate) {
+        super.setJdbcTemplate(jdbcTemplate);
+    }
+
+    /**
+     * Simple insert into our plugged in DB i presume..
+     * Wait, whats the advantage of this over an ORM again?
+     * @param event the entire event info.. (will probably be pointing to our future custom event view for this user..)
+     */
+    public void insert(final Event event) {
+        this.getJdbcTemplate().update(INSERT, new PreparedStatementSetter() {
+
+            @Override
+            public void setValues(PreparedStatement stmt) throws SQLException {
+                // question on system exceptions vs custom exception..
+                // i have found that custom exceptions have the following advantages
+                // 1. log searching becomes so much more easy
+                // 2. can distinguish between expected and unexpected errors.. thus helping stats n such
+                // maybe dumb, but we can have a whole array of custom exception levels and have global handlers..
+                // thoughts? now i am thinkin this is startin to sound more like aops
+                stmt.setString(1, event.getId().toString());
+                stmt.setString(2, event.getCorrelationId().toString());
+            }
+
+        });
+    }
+
+    /**
+     * Shall we make this a practice before the code complexifies?
+     * @param id the specific UUID id you are querying for
+     * @return a getNamedParameterJdbcTemplate query return (or something)
+     */
+    public Event find(UUID id) {
+        SqlParameterSource params = new MapSqlParameterSource("id", id.toString());
+
+        return this.getNamedParameterJdbcTemplate()
+                .queryForObject(FIND_BY_ID, params, new EventMapper());
+    }
+
 }
