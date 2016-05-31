a8b5a2cb645a1bdf670caf77abb9af356534ef96
Anthony C <acuevas.15@gmail.com>
2016-05-31 15:31:08 -0400

    removed redundant data from enum

--- app/src/main/java/slam/model/type/StatusType.java
+++ app/src/main/java/slam/model/type/StatusType.java
@@ -1,17 +1,5 @@
 package slam.model.type;
 
 public enum StatusType {
-	PENDING("PENDING"),
-	COMPLETE("COMPLETE");
-
-//	todo: i was just playing around.. some advice on enum usage from your experience please
-	private final String statusText;
-
-	StatusType(String status) {
-		this.statusText = status;
-	}
-
-	public String getStatusType() {
-		return this.statusText;
-	}
+	PENDING, COMPLETE;
 }
